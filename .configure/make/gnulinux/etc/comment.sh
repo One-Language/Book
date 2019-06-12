84525b9287c163c71dd8512f9c0a9aadd5ad2a73
Pglapds <46846354+pglapds@users.noreply.github.com>
2019-06-12 17:47:21 -0300

    Create comment.sh

---
+++ .configure/make/gnulinux/etc/comment.sh
@@ -0,0 +1,4 @@
+# https://stackoverflow.com/questions/31929621/echo-multiple-variables-in-same-line-bash
+# https://unix.stackexchange.com/questions/88452/concatenating-two-variables-with-an-underscore
+# https://www.linuxquestions.org/questions/linux-general-1/shell-script-displaying-multiple-variables-855232/
+# https://linuxhint.com/variables-bash-programming/
