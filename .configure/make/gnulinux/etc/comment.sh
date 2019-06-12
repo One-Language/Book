6b501351cc37e1cca7203d94eb3f385dff500f6f
Pglapds <46846354+pglapds@users.noreply.github.com>
2019-06-12 19:59:45 -0300

    Delete comment.sh

--- .configure/make/gnulinux/etc/comment.sh
+++
@@ -1,5 +0,0 @@
-# https://stackoverflow.com/questions/31929621/echo-multiple-variables-in-same-line-bash
-# https://unix.stackexchange.com/questions/88452/concatenating-two-variables-with-an-underscore
-# https://www.linuxquestions.org/questions/linux-general-1/shell-script-displaying-multiple-variables-855232/
-# https://linuxhint.com/variables-bash-programming/
-# https://linuxhint.com/bash_command_output_variable/
