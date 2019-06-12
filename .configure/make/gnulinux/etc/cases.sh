df312df014bd133d540b7a17cbc6a1309829e14f
Pglapds <46846354+pglapds@users.noreply.github.com>
2019-06-12 19:45:32 -0300

    Update cases.sh

--- .configure/make/gnulinux/etc/cases.sh
+++ .configure/make/gnulinux/etc/cases.sh
@@ -1,10 +1,1 @@
-# & OPTION 4 reference 
-  # @reference: https://superuser.com/questions/1260038/font-size-in-shell-script 
-  # @reference: https://www.cyberciti.biz/faq/create-large-colorful-text-banner-on-screen
-  # @reference: https://stackoverflow.com/questions/25036555/how-to-print-in-shell-script
-  # @reference: https://stackoverflow.com/questions/996158/modifying-font-size-using-shell-script
-  # @reference: https://www.unix.com/shell-programming-and-scripting/96575-change-font-size-bash.html
-  # @reference: https://it.toolbox.com/question/how-to-change-font-size-of-text-in-a-file-using-a-shell-script-071608                                                                    
-  # @reference: https://unix.stackexchange.com/questions/398766/how-do-i-increase-the-font-size-of-the-centos-7-console         
-  # @reference: https://unix.stackexchange.com/questions/77049/how-do-i-change-the-screen-font-size-when-using-a-virtual-console
-  # @reference: https://www.linuxquestions.org/questions/linux-newbie-8/can-i-increase-font-size-through-shell-scripts-4175502369
+-------
