6db241d12a1063ba5f1f2ab6690693ffd0c5fb57
Pglapds <46846354+pglapds@users.noreply.github.com>
2019-06-12 18:01:15 -0300

    Update install.sh

--- .configure/make/gnulinux/install.sh
+++ .configure/make/gnulinux/install.sh
@@ -16,28 +16,31 @@ echo # ouput: new line
 echo SPECIFIC CONTRIBUTION : book/*  # ouput: new line SPECIFIC CONTRIBUTION : book/* 
 echo @url https://github.com/ET-Lang/book/  # ouput: new line @url https://github.com/ET-Lang/book/
 
+# COMMAND PATH  - Show Directory and Path Here
+# --------------------------------------------------------------------------------------------------------------------
+$path=`pwd`
+echo "& path $path"
+
 # COMMAND VERSION 
 # --------------------------------------------------------------------------------------------------------------------
-(
-echo "OS                      :  Gnulinux"
-echo "License                 :  Copyright, ET-STD, ET-BOOK ... "
-echo "Reference               :  https://github.com/ET-Lang/book "
-echo "Version, Year, Date     :  2019 , Wednesday, June 12th 2019 @ 01:47:23 PM https://github.com/ET-Lang/book"
-)
+v1 = echo "OS                      :  Gnulinux"
+v2 = echo "License                 :  Copyright, ET-STD, ET-BOOK ... "
+v3 = echo "Reference               :  https://github.com/ET-Lang/book "
+v4 = echo "Version, Year, Date     :  2019 , Wednesday, June 12th 2019 @ 01:47:23 PM https://github.com/ET-Lang/book"
+echo "${V1, V2, V3, V4}"
 
 
 # COMMAND REFERENCE
 # --------------------------------------------------------------------------------------------------------------------
 
-reference=$(ls \
-   -1)
-echo "&{reference}"
+#reference=$(ls \
+ #  -1)
+#echo "&{reference}"
 
 
 # COMMAND INSTALL FOR AUTO DOWNLOAD, UNPACK, EXTRACT, AUTO GENERATE PDF
 # --------------------------------------------------------------------------------------------------------------------
 
-> https://github.com/shelljs/shelljs
 
 > wget https://codeload.github.com/ET-Lang/book/zip/master
 
