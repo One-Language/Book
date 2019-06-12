066ab395b0903d716dab7e05c1fc8e126ed0e245
Pglapds <46846354+pglapds@users.noreply.github.com>
2019-06-12 16:24:10 -0300

    Update install.sh

--- .configure/make/gnulinux/install.sh
+++ .configure/make/gnulinux/install.sh
@@ -1,9 +1,63 @@
+#!/bin/sh
 
 "& BOOK"
 
-@OPTION figlet
-@OPTION toilet
-@OPTION dcop 
+msg1=&
+ms="&msg1 msg1"
+msg2="$ms two"
+msg3="$msg2 three"
+echo $msg3
+
+# Author @pglapds
+# @url https://github.com/ET-Lang/book/ 
+
+# CONTRIBUTOR @pglapds 
+# @url https://github.com/ET-Lang/book/ 
+
+# CONTRIBUTION 
+# @url https://github.com/ET-Lang/book/ 
+
+# SPECIFIC CONTRIBUTION : book/*  
+# @url https://github.com/ET-Lang/book/ 
+
+
+# OS                      :  Gnulinux
+
+# License                 :  Copyright, ET-STD, ET-BOOK ... 
+# Reference               :  https://github.com/ET-Lang/book
+# Version, Year, Date     :  2019 , Wednesday, June 12th 2019 @ 01:47:23 PM https://github.com/ET-Lang/book
+                 
+# OUTPUT
+
+# OS                      :  Gnulinux
+
+# License                 :  Copyright, ET-STD, ET-BOOK ... 
+# Reference               :  https://github.com/ET-Lang/book
+# Version, Year, Date     :  2019 , Wednesday, June 12th 2019 @ 01:47:23 PM https://github.com/ET-Lang/book
+      
+
+# @OPTION 1 is figlet
+# @OPTION 2 is toilet
+# @OPTION 3 is dcop 
+# @OPTION 4 is Shell Script 
+# @OPTION 5 is Nodejs 
+
+# OUTPUT
+
+# @OPTION 1 ' > $ figlet & BOOK'
+# if select @OPTION figlet , ' > $ figlet & BOOK'
+
+# @OPTION toilet ,   ' > $ figlet & BOOK'
+# if select @OPTION figlet , ' > $ figlet & BOOK'
+
+# @OPTION toilet ,   ' > $ figlet & BOOK'
+# if select @OPTION figlet , ' > $ figlet & BOOK'
+
+
+# @OPTION toilet ,   ' > $ figlet & BOOK'
+# if select @OPTION figlet , ' > $ figlet & BOOK'
+
+# @OPTION dcop 
 
 > $ sudo apt-get update
 > $ sudo apt-get install toilet figlet
@@ -15,23 +69,38 @@
 > & OPTION 1 -h 
 > & OPTION 2 -h 
 > & OPTION 3 -h 
-> & OPTION 4 -h 
+> & OPTION 4 reference
 
 ! dcop konsole-8540 session-6 setFont Monospace,100,-1,5,50,0,0,0,0,0
 ! 
 
-@reference: https://superuser.com/questions/1260038/font-size-in-shell-script      
-@reference: https://www.cyberciti.biz/faq/create-large-colorful-text-banner-on-screen/
-@reference: https://stackoverflow.com/questions/996158/modifying-font-size-using-shell-script
-@reference: https://unix.stackexchange.com/questions/398766/how-do-i-increase-the-font-size-of-the-centos-7-console                                                                    
-@reference: https://unix.stackexchange.com/questions/77049/how-do-i-change-the-screen-font-size-when-using-a-virtual-console
-@reference: https://www.linuxquestions.org/questions/linux-newbie-8/can-i-increase-font-size-through-shell-scripts-4175502369/                                                                   
-                                                                    
-                                                                    
-wget https://codeload.github.com/ET-Lang/book/zip/master
+echo " & @reference: https://superuser.com/questions/1260038/font-size-in-shell-script"   
+echo " & @reference: https://www.cyberciti.biz/faq/create-large-colorful-text-banner-on-screen"
+echo " & @reference: https://stackoverflow.com/questions/25036555/how-to-print-in-shell-script"
+echo " & @reference: https://stackoverflow.com/questions/996158/modifying-font-size-using-shell-script"
+echo " & @reference: https://www.unix.com/shell-programming-and-scripting/96575-change-font-size-bash.html"
+echo " & @reference: https://it.toolbox.com/question/how-to-change-font-size-of-text-in-a-file-using-a-shell-script-071608"                                                                    
+echo " & @reference: https://unix.stackexchange.com/questions/398766/how-do-i-increase-the-font-size-of-the-centos-7-console"         
+echo " & @reference: https://unix.stackexchange.com/questions/77049/how-do-i-change-the-screen-font-size-when-using-a-virtual-console"
+echo " & @reference: https://www.linuxquestions.org/questions/linux-newbie-8/can-i-increase-font-size-through-shell-scripts-4175502369"
 
-unzip book.zip 
+# OUTPUT
 
-cd book/src/content/en-us
+# & OPTION 4 reference 
+  # & @reference: https://superuser.com/questions/1260038/font-size-in-shell-script 
+  # & @reference: https://www.cyberciti.biz/faq/create-large-colorful-text-banner-on-screen
+  # & @reference: https://stackoverflow.com/questions/25036555/how-to-print-in-shell-script
+  # & @reference: https://stackoverflow.com/questions/996158/modifying-font-size-using-shell-script
+  # & @reference: https://www.unix.com/shell-programming-and-scripting/96575-change-font-size-bash.html
+  # & @reference: https://it.toolbox.com/question/how-to-change-font-size-of-text-in-a-file-using-a-shell-script-071608                                                                    
+  # & @reference: https://unix.stackexchange.com/questions/398766/how-do-i-increase-the-font-size-of-the-centos-7-console         
+  # & @reference: https://unix.stackexchange.com/questions/77049/how-do-i-change-the-screen-font-size-when-using-a-virtual-console
+  # & @reference: https://www.linuxquestions.org/questions/linux-newbie-8/can-i-increase-font-size-through-shell-scripts-4175502369
+                                                        
+> wget https://codeload.github.com/ET-Lang/book/zip/master
 
-pdflatex main.tex
+> unzip book.zip 
+
+> cd book/src/content/en-us
+
+> pdflatex main.tex
