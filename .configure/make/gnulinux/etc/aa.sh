7dde92053c9de3163053914beaf25865934ec7b1
Pglapds <46846354+pglapds@users.noreply.github.com>
2019-06-12 18:57:16 -0300

    Create aa.sh

---
+++ .configure/make/gnulinux/etc/aa.sh
@@ -0,0 +1,4 @@
+https://tecadmin.net/pass-command-line-arguments-in-shell-script/
+https://askubuntu.com/questions/33252/how-to-make-my-own-command-with-parameters
+https://unix.stackexchange.com/questions/84686/how-to-create-custom-commands-in-unix-linux
+https://www.quora.com/How-do-I-make-my-own-command-in-Linux
