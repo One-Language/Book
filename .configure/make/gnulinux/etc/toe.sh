5c9f38d9592e44a5f00670c63ab7e407b0c97429
Pglapds <46846354+pglapds@users.noreply.github.com>
2019-06-12 18:06:29 -0300

    Update toe.sh

--- .configure/make/gnulinux/etc/toe.sh
+++ .configure/make/gnulinux/etc/toe.sh
@@ -1,1 +1,6 @@
 https://github.com/shelljs/shelljs
+https://www.linuxquestions.org/questions/linux-general-1/shell-script-displaying-multiple-variables-855232/
+https://unix.stackexchange.com/questions/88452/concatenating-two-variables-with-an-underscore
+https://askubuntu.com/questions/1066396/how-to-print-multiline-variables-in-side-by-side-columns-bash
+https://linuxhint.com/bash_command_output_variable/
+https://linuxhint.com/variables-bash-programming/
