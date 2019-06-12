2948f7cc2ebdbb9c5ee0176c11709d934e7e84b7
Pglapds <46846354+pglapds@users.noreply.github.com>
2019-06-12 17:34:05 -0300

    Create build.sh

---
+++ .configure/make/gnulinux/build.sh
@@ -0,0 +1,41 @@
+
+# ECHO BUILD : OUTPUT
+# --------------------------------------------------------------------------------------------------------------------
+
+# & Book!
+
+# Author @pglapds
+# @url https://github.com/ET-Lang/book/ 
+
+# CONTRIBUTOR @pglapds 
+# @url https://github.com/ET-Lang/book/ 
+
+# CONTRIBUTION 
+# @url https://github.com/ET-Lang/book/ 
+
+# SPECIFIC CONTRIBUTION : book/*  
+# @url https://github.com/ET-Lang/book/ 
+
+# CLEAR 
+# --------------------------------------------------------------------------------------------------------------------
+
+# OS                      :  Gnulinux
+
+# License                 :  Copyright, ET-STD, ET-BOOK ... 
+# Reference               :  https://github.com/ET-Lang/book
+# Version, Year, Date     :  2019 , Wednesday, June 12th 2019 @ 01:47:23 PM https://github.com/ET-Lang/book
+# --------------------------------------------------------------------------------------------------------------------
+      
+# & OPTION 1 /NONE
+# & OPTION 2 /NONE
+# & OPTION 3 /NONE
+# & OPTION 4 /NONE
+# & OPTION 5 /NONE
+# & OPTION 6 /WORK
+# --------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
