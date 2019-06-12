13f743e60b9a2fe387ec121017bd93023e5bdd44
Pglapds <46846354+pglapds@users.noreply.github.com>
2019-06-12 17:29:03 -0300

    Update readme.sh

--- .configure/make/gnulinux/readme.sh
+++ .configure/make/gnulinux/readme.sh
@@ -1,13 +1,29 @@
-# ----------------------------------------------
+# WARNINGS !
+
+# @OPTION 1 is figlet status soon 
+# @OPTION 1 ' > $ figlet & BOOK'
+# if select @OPTION figlet
+
+# @OPTION 2 is toilet status soon 
+# @OPTION 2 ' > $ toilet & BOOK'
+# if select @OPTION 2 toilet
+
+# REQUIRE AND USE 
+# $ sudo apt-get update
+# $ sudo apt-get install toilet figlet
+# $ figlet & BOOK
+# $ figlist -h 
+
 
 # @OPTION 3 is dcop status soon 
+# ----------------------------------------------
 # @OPTION 3 ' > $ dcop & BOOK'
 # dcop konsole-8540 session-6 setFont Monospace,100,-1,5,50,0,0,0,0,0
 # if select @OPTION 3 dcop utility  
 
-# ----------------------------------------------
 
 # @OPTION 4 is Nodejs soon 
+# ----------------------------------------------
 # @OPTION 4 ' > $ nodejs book.js'
 # if select @OPTION 4 nodejs 
 
@@ -16,74 +32,86 @@
 # https://www.npmjs.com/package/font-size
 # https://stackoverflow.com/questions/26522854/console-log-font-size-in-nodejs
 
-# ----------------------------------------------
-
 # @OPTION 5 is Python soon 
+# ----------------------------------------------
 # @OPTION 5 ' > $ python book.py'
 # if select @OPTION 5 Python 
 
-# ----------------------------------------------
-
 # @OPTION 6 is Shell Script  Not complete yet
+# ----------------------------------------------
 # if select @OPTION 6 Shell Script 
 
-# SHELL SCRIPT, REQUIRES
 
-# MODEL
+# SHELL SCRIPT, REQUIRES
+# ----------------------------------------------
+
+# 1- MODEL
 # msg1 = &
 # ms="&msg1 msg1"
 # msg2="&ms two"
 # msg3="&msg2 three"
 # echo &msg3
 
-# SCREEN DESIGN - BUILD ^^ commandline options and parameters
+# 2- SCREEN DESIGN - BUILD ^^ commandline options and parameters
 
 # @OPTION 1 figlet 
-# --------------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------
 # & OPTION 1
 # & OPTION 1 -h 
 # & OPTION 1 -r
  
 # @OPTION 2 toilet 
-# --------------------------------------------------------------------------------------------------------------------
-
+# ----------------------------------------------
 # & OPTION 2 
 # & OPTION 2 -h 
 # & OPTION 2 -r
 
 # @OPTION 3 dcop 
+# ----------------------------------------------
 # & OPTION 3
 # & OPTION 3 -h 
 # & OPTION 3 -r
 
 # @OPTION 4 Nodejs 
-# --------------------------------------------------------------------------------------------------------------------
-
+# ----------------------------------------------
 # & OPTION 4 
 # & OPTION 4 -h 
 # & OPTION 4 -r
 
 # @OPTION 5 Python 
-# --------------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------
 # & OPTION 5
 # & OPTION 5 -h 
 # & OPTION 5 -r
 
 # @OPTION 6 Shell Script 
-# --------------------------------------------------------------------------------------------------------------------
-
+# ----------------------------------------------
 # & OPTION 6
 # & OPTION 6 -h 
 # & OPTION 6 -r
 
+
+3- STRUCT AND DESIGN
+
 # & -h is help 
 # & -r is running
+# & -version is version
 
 # & OPTION 1 -h -r
-# & OPTION 2 -h -r
-# & OPTION 3 -h -r
-# & OPTION 4 -h -r
-# & OPTION 5 -h -r
+# ----------------------------------------------
 
-# & OPTION 6 -h -r reference
-# --------------------------------------------------------------------------------------------------------------------
+# & OPTION 2 -h -r
+# ----------------------------------------------
+
+# & OPTION 3 -h -r
+# ----------------------------------------------
+
+# & OPTION 4 -h -r
+# ----------------------------------------------
+
+# & OPTION 5 -h -r
+# ----------------------------------------------
+
+# & OPTION 6 -version -r -h reference
+# ----------------------------------------------
+
