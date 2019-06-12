8d4296e0a5fb492bc32c787cf63564894368c448
Pglapds <46846354+pglapds@users.noreply.github.com>
2019-06-12 18:19:17 -0300

    Update tsdsds.sh

--- .configure/make/gnulinux/etc/tsdsds.sh
+++ .configure/make/gnulinux/etc/tsdsds.sh
@@ -1,1 +1,4 @@
 http://linuxcommand.org/lc3_wss0010.php
+https://medium.com/tech-tajawal/writing-shell-scripts-the-beginners-guide-4778e2c4f609
+https://superuser.com/questions/536496/capturing-the-console-output
+https://www.cyberciti.biz/faq/how-to-write-output-to-terminal/
