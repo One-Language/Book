8b8437ff630659d4131b36fd526e9356e0a4380c
Pglapds <46846354+pglapds@users.noreply.github.com>
2019-06-12 17:29:49 -0300

    Create warnings.sh

---
+++ .configure/make/gnulinux/warnings.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# #!/bin/bash 
+
+# WARNINGS !
+
+# @OPTION 1 is figlet status soon 
+# @OPTION 1 ' > $ figlet & BOOK'
+# if select @OPTION figlet
+
+# @OPTION 2 is toilet status soon 
+# @OPTION 2 ' > $ toilet & BOOK'
+# if select @OPTION 2 toilet
+
+# REQUIRE AND USE 
+# $ sudo apt-get update
+# $ sudo apt-get install toilet figlet
+# $ figlet & BOOK
+# $ figlist -h 
