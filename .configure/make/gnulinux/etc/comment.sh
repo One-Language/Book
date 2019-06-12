684c0bb9713fbf021654dc8d98afe1f2f1a20eaf
Pglapds <46846354+pglapds@users.noreply.github.com>
2019-06-12 17:48:39 -0300

    Update comment.sh

--- .configure/make/gnulinux/etc/comment.sh
+++ .configure/make/gnulinux/etc/comment.sh
@@ -2,3 +2,4 @@
 # https://unix.stackexchange.com/questions/88452/concatenating-two-variables-with-an-underscore
 # https://www.linuxquestions.org/questions/linux-general-1/shell-script-displaying-multiple-variables-855232/
 # https://linuxhint.com/variables-bash-programming/
+# https://linuxhint.com/bash_command_output_variable/
