5a43ff3da709e051ecb5cf7c401f13c49122f46c
Pglapds <46846354+pglapds@users.noreply.github.com>
2019-06-12 22:58:56 -0300

    Update build-readme.sh

--- .configure/make/gnulinux/etc/build-readme.sh
+++ .configure/make/gnulinux/etc/build-readme.sh
@@ -1,107 +1,94 @@
-#!/bin/sh
-# #!/bin/bash 
-
+#
+#
 # STEP 0- RESEARCH 
-
+#
+#
 # STEP 1- DESIGN, OUTPUT
+#
+#
+# STEP 2- STRUCT AND DESIGN
+#
+#
+# STEP 3- SCREEN DESIGN - BUILD ^^ commandline options and parameters
+#
+#
+# @BASE http://www.devin.com.br/shell_script/
+# @BASE https://terminalroot.com.br/2013/07/shell-scripitingbash-funcao-para.html
+# @BASE
+#
+#
+# Declare Shell: #!/bin/sh , #!/bin/csh , #!/bin/bash 
+#
+#
 
-# @OPTION 3 is dcop status soon 
+# @OPTION 1 Nodejs 
 # ----------------------------------------------
-# @OPTION 3 ' > $ dcop & BOOK'
-# dcop konsole-8540 session-6 setFont Monospace,100,-1,5,50,0,0,0,0,0
-# if select @OPTION 3 dcop utility  
+# & OPTION1 
+# & OPTION1 -h 
+# & OPTION1 -r
 
-
-# @OPTION 4 is Nodejs soon 
+# @OPTION 2 Python 
 # ----------------------------------------------
-# @OPTION 4 ' > $ nodejs book.js'
-# if select @OPTION 4 nodejs 
-
-# NODEJS, REQUIRES
-# https://www.npmjs.com/package/figlet
-# https://www.npmjs.com/package/font-size
-# https://stackoverflow.com/questions/26522854/console-log-font-size-in-nodejs
-
-# @OPTION 5 is Python soon 
-# ----------------------------------------------
-# @OPTION 5 ' > $ python book.py'
-# if select @OPTION 5 Python 
-
-# @OPTION 6 is Shell Script  Not complete yet
-# ----------------------------------------------
-# if select @OPTION 6 Shell Script 
+# & OPTION2
+# & OPTION2 -h 
+# & OPTION2 -r
 
 
-# SHELL SCRIPT, REQUIRES
-# ----------------------------------------------
+# & VERSION   ( 1- ECHO BUILD  )
+# & LICENSE   ( 2- ECHO BUILD  )
+# & REFERENCE ( 3- ECHO BUILD  )
 
-# 1- MODEL
-# msg1 = &
-# ms="&msg1 msg1"
-# msg2="&ms two"
-# msg3="&msg2 three"
-# echo &msg3
+# & -install is INSTALL 
+# & -h is HELP 
+# & -r is RUNNING
 
-# STEP 2-  SCREEN DESIGN - BUILD ^^ commandline options and parameters
+# NEW RESOURCES :
+# -
 
-# @OPTION 1 figlet 
-# ----------------------------------------------
-# & OPTION 1
-# & OPTION 1 -h 
-# & OPTION 1 -r
- 
-# @OPTION 2 toilet 
-# ----------------------------------------------
-# & OPTION 2 
-# & OPTION 2 -h 
-# & OPTION 2 -r
+# - // OPTION1() {   #  NEW OPTION1 - INSTALL WITH Python  ( Shell Scripiting(Bash): &  OPTION1 -install -version ( 1- ECHO BUILD  ) -r ( running ) -h ( help ) reference ( 2- ECHO BUILD  ) )
+# - // ' > $ python book.py'
+# - // if select @OPTION 1 - Python 
+# - // Python
+# - // etBOOK
+# - //
+# - // }
 
-# @OPTION 3 dcop 
-# ----------------------------------------------
-# & OPTION 3
-# & OPTION 3 -h 
-# & OPTION 3 -r
+# - // OPTION2() {  #  NEW OPTION2 - INSTALL WITH Nodejs ( Shell Scripiting(Bash): &  OPTION2 -install -version ( 1- ECHO BUILD  ) -r ( running ) -h ( help ) reference ( 2- ECHO BUILD  ) )
+# - // ' > $ nodejs book.js'
+# - // if select @OPTION 4 - Nodejs 
+# - //
+# - // REQUIRES
+# - // https://www.npmjs.com/package/figlet
+# - // https://www.npmjs.com/package/font-size
+# - // https://stackoverflow.com/questions/26522854/console-log-font-size-in-nodejs
+# - //
+# - // etBOOK
+# - //
+# - // }
 
-# @OPTION 4 Nodejs 
-# ----------------------------------------------
-# & OPTION 4 
-# & OPTION 4 -h 
-# & OPTION 4 -r
+# - // OPTION3() {  #  NEW OPTION 3 - SHOW WITH Figlet ( Shell Scripiting(Bash):  &  OPTION3 -install -version ( 1- ECHO BUILD  ) -r ( running ) -h ( help ) reference ( 2- ECHO BUILD  ) )
+# - //
+# - // if select @OPTION 3 - Figlet
+# - //
+# - // etBOOK
+# - //
+# - // }
 
-# @OPTION 5 Python 
-# ----------------------------------------------
-# & OPTION 5
-# & OPTION 5 -h 
-# & OPTION 5 -r
+# - // OPTION4() {  # NEW OPTION 4 - SHOW WITH Toilet ( Shell Scripiting(Bash):  &  OPTION4 -install -version ( 1- ECHO BUILD  ) -r ( running ) -h ( help ) reference ( 2- ECHO BUILD  ) )
+# - //
+# - // if select @OPTION 4 - Toilet 
+# - // Toilet
+# - // etBOOK
+# - //
+# - // }
 
-# @OPTION 6 Shell Script 
-# ----------------------------------------------
-# & OPTION 6
-# & OPTION 6 -h 
-# & OPTION 6 -r
+# - // OPTION5() {  # NEW OPTION 5 - SHOW WITH Dcop ( Shell Scripiting(Bash):  &  OPTION5 -install -version ( 1- ECHO BUILD  ) -r ( running ) -h ( help ) reference ( 2- ECHO BUILD  )  )
+# - // if select @OPTION 5 - Dcop
+# - // dcop konsole-8540 session-6 setFont Monospace,100,-1,5,50,0,0,0,0,0
+# - // Dcop 
+# - // etBOOK
+# - // }
 
 
-# STEP 3- STRUCT AND DESIGN
 
-# & -h is help 
-# & -r is running
-# & -version is version
-
-# & OPTION 1 -h -r
-# ----------------------------------------------
-
-# & OPTION 2 -h -r
-# ----------------------------------------------
-
-# & OPTION 3 -h -r
-# ----------------------------------------------
-
-# & OPTION 4 -h -r
-# ----------------------------------------------
-
-# & OPTION 5 -h -r
-# ----------------------------------------------
-
-# & OPTION 6 -version -r -h reference
-# ----------------------------------------------
 
