d2abe23a1a3c032fe217abffb92f11545c1e4c7c
Pglapds <46846354+pglapds@users.noreply.github.com>
2019-06-13 00:12:40 -0300

    Update main.sh

--- .configure/make/gnulinux/main.sh
+++ .configure/make/gnulinux/main.sh
@@ -1,6 +1,37 @@
+# - 
 # import bash 
+# - 
 #!/bin/bash
 
-export PATH=$PATH:/home/@username/etBook
+# -
+# - Author:  @contribution
+# - License: & Book 2019 , © https://github.com/ET-Lang/book/ 
+# -
+# -
+# - Shell Scripiting(Bash): Installation Automatic Process
+# - @use: & OPTION1 , OPTION2, OPTION3, OPTION4 ...   
+# -
+# -
+# - Shell Scripiting(Bash): Installation Automatic Process
+# - @use: & -install -version ( 1- ECHO BUILD  ) -r ( running ) -h ( help ) reference ( 2- ECHO BUILD  ) 
+# -
 
-chmod +x main.sh
+# -
+# - 
+# 0 - ECHO BUILD
+# - 
+# -
+echo # ouput: new line
+echo "& Book!" # ouput: new line: Sample in browser Javascript : console.log("& Book!")
+echo "-" # ouput: new line
+echo # ouput: new line
+echo "progress 90%:"
+echo # ouput: new line
+echo # ouput: new line
+echo # ouput: new line
+
+# - start chmod +x main.sh 
+$( chmod +x main.sh )
+
+# - start export PATH=$PATH:/home/@username/etBook
+$( export PATH=$PATH:/home/@username/etBook )
